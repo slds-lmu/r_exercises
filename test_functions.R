f56b02240ddc9607568f9e3897d0592277177f07
Laetitia Frost <51784216+laefrost@users.noreply.github.com>
2022-09-05 16:22:48 +0200

    Add files via upload

---
+++ test_functions.R
@@ -0,0 +1,174 @@
+### Endpoint for various checkerfunctions
+### Relevant input Arguments:
+### label = String corresponding to the name of the chunk from which the checker is calles (e.g. "task")
+### user_code = String containing the user code
+### solution_code = String containing the code from the solution chunk
+### envir_result = environment containing all the global variables from the set up chunks and the variables defined in the current chunk
+### last_value = last_value of exercise chunk
+checker_endpoint <-
+  function(label = exercise$label,
+           user_code = exercise$code,
+           solution_code = exercise$solution,
+           check_code = exercise$check,
+           envir_result = envir,
+           evaluate_result = evaluate_result,
+           envir_prep = envir_prep,
+           last_value = last_value,
+           ...) {
+    print(label)
+    if (label == "task") {
+      return(checker_task(envir_result, german))
+    }
+    else if (label == "learner") {
+      return(checker_learner(envir_result))
+    }
+    else if (label == "predict") {
+      return(checker_predict(user_code, solution_code, envir_result))
+    }
+    else if (label == "train") {
+      return(checker_train(user_code, solution_code, envir_result))
+    }
+  }
+
+
+checker_task <-
+  function(envir, backend) {
+    print(ls(envir))
+    task_classif <- envir$taskClassifSol
+    bool_correctness <- TRUE
+    message <- "Everything looks fine! Run your code!"
+    # check if variable "taskClassif" (=task specified by user) is in exercise environment
+    if (!("taskClassif" %in% ls(envir))) {
+      message <-
+        "Make sure you use the variable name specified in the exercise chunk!"
+      bool_correctness <- FALSE
+      return(list(
+        message = message,
+        correct = bool_correctness,
+        location = "append"
+      ))
+    }
+    task <- envir$taskClassif
+    # check if task-Variable is of class Task
+    if (!("Task" %in% class(task))) {
+      return(
+        list(
+          message = "Make sure you initialize an object of mlr3 class Task",
+          correct = FALSE,
+          location = "append"
+        )
+      )
+    }
+    data <-
+      as.data.frame(task$backend$data(
+        rows = seq_len(nrow(backend)),
+        cols = colnames(backend),
+        data_format = "data.table"
+      ))
+    # check backend/data of task -> has to be done this way as data()- is a function and not necessarily the attribute to retrieve all data of the backend
+    if (!identical(data, backend)) {
+      bool_correctness <- FALSE
+      message <-
+        "There is something wrong with the backend you've specified. Make sure to use the correct data set!"
+    }
+    ## check target of task
+    if (task$target_names != task_classif$target_names) {
+      bool_correctness <- FALSE
+      message <-
+        "There is something wrong with the backend you've specified. Make sure to use the correct column as target!"
+    }
+    return(list(
+      message = message,
+      correct = bool_correctness,
+      location = "append"
+    ))
+
+  }
+
+checker_learner <- function(envir) {
+  print(ls(envir))
+  bool_correctness <- TRUE
+  message <- "Everything looks fine! Run your code!"
+  if (!("lrnClassif" %in% ls(envir))) {
+    message <-
+      "Make sure you use the variable name specified in the exercise chunk!"
+    bool_correctness <- FALSE
+    return(list(
+      message = message,
+      correct = bool_correctness,
+      location = "append"
+    ))
+  }
+  learner <- envir$lrnClassif
+  # check if learner-Variable is of class Learner
+  if (!("Learner" %in% class(learner))) {
+    return(
+      list(
+        message = "Make sure you initialize an object of mlr3 learner class",
+        correct = FALSE,
+        location = "append"
+      )
+    )
+  }
+  ### check learner
+  if (!(class(learner) == class(envir$lrnClassifSol))) {
+    bool_correctness <- FALSE
+    message <-
+      "There is something wrong with the learner you've specified"
+  }
+  return(list(
+    message = message,
+    correct = bool_correctness,
+    location = "append"
+  ))
+}
+
+checker_predict <-
+  function(user_code,
+           solution_code, envir) {
+    print(ls(envir))
+    if (user_code == solution_code) {
+      return(
+        list(
+          message = "Everything looks fine! Run your code!",
+          correct = TRUE,
+          location = "append"
+        )
+      )
+    }
+    else {
+      return(
+        list(
+          message = "There is something wrong with your function. Check for spelling errors or if your have used the correct data for prediction!",
+          correct = FALSE,
+          location = "append"
+        )
+      )
+    }
+  }
+
+checker_train <-
+  function(user_code,
+           solution_code, envir) {
+    print(ls(envir))
+    print(user_code)
+    print(solution_code)
+    if (user_code %in% solution_code) {
+      return(
+        list(
+          message = "Everything looks fine! Run your code!",
+          correct = TRUE,
+          location = "append"
+        )
+      )
+    }
+    else {
+      return(
+        list(
+          message = "There is something wrong with your function. Check for spelling errors or if your have used the correct data for training!",
+          correct = FALSE,
+          location = "append"
+        )
+      )
+    }
+  }
